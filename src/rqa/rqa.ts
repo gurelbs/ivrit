1e23f6b25aaa067895ea579c33576cbd9990cced
gurelbs <guri240@gmail.com>
2021-10-10 22:09:15 +0300

    'updated'

--- src/rqa/rqa.ts
+++ src/rqa/rqa.ts
@@ -1,22 +1,24 @@
 import puppeteer from 'puppeteer';
 const CATCH = new Map();
-import {dictionary,Dictionary} from './../dictionary/dictionary'
-import {getNews} from './../news/getNews'
-function fixedString(q:string, dic: Dictionary[]){
-  for (const {word,replaceWith} of dic){
-    const replaceIt = (typeof word === 'string' && q.includes(word)) || q.match(word)
-    if (replaceIt){
-      q = q.replace(word,replaceWith).trim()
+import { dictionary, Dictionary } from './../dictionary/dictionary';
+import { getNews } from './../news/getNews';
+function fixedString(q: string, dic: Dictionary[]) {
+  for (const { word, replaceWith } of dic) {
+    const replaceIt = (typeof word === 'string' && q.includes(word)) || q.match(word);
+    if (replaceIt) {
+      q = q.replace(word, replaceWith).trim();
     }
   }
-  return q
+  return q;
 }
 export async function rqa(question: string): Promise<string | undefined> {
   if (question.includes('חדשות')) {
-    question = question.replace('חדשות','')
+    question = question.replace('חדשות', '');
     const news = await getNews(question);
-    const newsString = Array.from(news).map(({header,time,origin}:any) => `${header}\n${origin} - ${time}.`).slice(0,5).join('\n')
-    return newsString
+    return Array.from(news)
+      .map(({ header, time, origin, link }: any) => `<a href="${link}">${header}\n${origin} - ${time}.</a>`)
+      .slice(0, 5)
+      .join('\n');
   }
   const url: string = `https://google.com/search?q=${question}&hl=he`;
   const res: string = '';
@@ -44,7 +46,7 @@ export async function rqa(question: string): Promise<string | undefined> {
           link: (document?.querySelector('.g.mnr-c.g-blk .g a') as HTMLLinkElement).href,
         }));
         if (result) {
-          return fixedString(result.answer,dictionary);
+          return fixedString(result.answer, dictionary);
         }
       }
     } catch (e) {
@@ -58,7 +60,7 @@ export async function rqa(question: string): Promise<string | undefined> {
           answer: (document?.querySelector('.kp-header') as HTMLElement).innerText.replace('/', ': '),
         }));
         if (kpHeader && !kpHeader.answer.includes(`תוצאת מידע\n`)) {
-          return fixedString(kpHeader.answer,dictionary);
+          return fixedString(kpHeader.answer, dictionary);
         }
       }
     } catch (e) {
@@ -76,7 +78,7 @@ export async function rqa(question: string): Promise<string | undefined> {
             .slice(0, 8),
         }));
         if (wikiData) {
-          return fixedString(wikiData.answer.join('\n'),dictionary);
+          return fixedString(wikiData.answer.join('\n'), dictionary);
         }
       }
     } catch (e) {
@@ -87,14 +89,16 @@ export async function rqa(question: string): Promise<string | undefined> {
       const moviesDOM = await page.$('[data-attrid="kc:/people/person:movies"]');
       if (moviesDOM) {
         const details = await page.evaluate(() => {
-          return fixedString([
-            ...(document?.querySelectorAll(
-              '[data-attrid="kc:/people/person:movies"] > div span',
-            ) as NodeListOf<HTMLElement>),
-          ]
-            .map((x) => x.innerText)
-            .join('')
-            ,dictionary);
+          return fixedString(
+            [
+              ...(document?.querySelectorAll(
+                '[data-attrid="kc:/people/person:movies"] > div span',
+              ) as NodeListOf<HTMLElement>),
+            ]
+              .map((x) => x.innerText)
+              .join(''),
+            dictionary,
+          );
         });
         const movies = await page.evaluate(() => {
           return [
@@ -126,21 +130,18 @@ export async function rqa(question: string): Promise<string | undefined> {
       if (cardSection || gCardSection) {
         if (cardSection) {
           const details = await page.evaluate(() => {
-            return (document?.querySelector('.card-section') as HTMLElement)?.innerText
-              .split('\n')
-              .slice(0, 3);
+            return (document?.querySelector('.card-section') as HTMLElement)?.innerText.split('\n').slice(0, 3);
           });
           if (details) {
-            return fixedString(details.join('\n'),dictionary);
+            return fixedString(details.join('\n'), dictionary);
           }
         }
         if (gCardSection) {
           const details = await page.evaluate(() => {
-            return (document?.querySelector('g-card-section [data-attrid]') as HTMLElement)?.innerText
-              .split('\n');
+            return (document?.querySelector('g-card-section [data-attrid]') as HTMLElement)?.innerText.split('\n');
           });
           if (details) {
-            return fixedString(details.join('\n'),dictionary);
+            return fixedString(details.join('\n'), dictionary);
           }
         }
       }
@@ -157,7 +158,7 @@ export async function rqa(question: string): Promise<string | undefined> {
             return (document?.querySelector('w-answer') as HTMLElement)?.innerText;
           });
           if (answer) {
-            return  fixedString(answer,dictionary);
+            return fixedString(answer, dictionary);
           }
         }
         if (wAnswerDesktop) {
@@ -166,7 +167,7 @@ export async function rqa(question: string): Promise<string | undefined> {
               return (document?.querySelector('w-answer-desktop') as HTMLElement)?.innerText;
             });
             if (answer) {
-              return  fixedString(answer,dictionary);
+              return fixedString(answer, dictionary);
             }
           }
         }
@@ -194,12 +195,12 @@ export async function rqa(question: string): Promise<string | undefined> {
           }));
           if (translate && translate.translateLyrics) {
             const str = result.songName + '\n' + result.singer + '\n' + result.lyric + '\n' + translate.translateLyrics;
-            return fixedString(str,dictionary);
+            return fixedString(str, dictionary);
           }
         } else {
           if (result) {
             const str = result.songName + '\n' + result.singer + '\n' + result.lyric;
-            return fixedString(str,dictionary);
+            return fixedString(str, dictionary);
           }
         }
       }
@@ -214,7 +215,7 @@ export async function rqa(question: string): Promise<string | undefined> {
           translate: (document?.querySelector('#tw-target-text') as HTMLElement).innerText,
         }));
         if (result && result.translate) {
-          return fixedString(result.translate,dictionary);
+          return fixedString(result.translate, dictionary);
         }
       }
     } catch (e) {
@@ -225,15 +226,14 @@ export async function rqa(question: string): Promise<string | undefined> {
     await context.close();
     await page.close();
     await browser.close();
-    if (res){
-      console.log(fixedString(res,dictionary));
-      return fixedString(res,dictionary)
+    if (res) {
+      console.log(fixedString(res, dictionary));
+      return fixedString(res, dictionary);
     } else {
-      return err
+      return err;
     }
   } catch (error) {
-    if (typeof  error)
-    console.log(error);
-    return err
+    if (typeof error) console.log(error);
+    return err;
   }
 }
